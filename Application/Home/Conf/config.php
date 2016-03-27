462728116084d5c5c67b23f56ccc7e178d4d8618
huhongyu <hilary.hu@qq.com>
2016-03-27 13:29:53 +0800

    shishi

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+  
+	
+	'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'localhost',
+	'DB_NAME'=>'time_count',//设置数据库名；
+	'DB_user'=>'root',
+	'DB_PWD'=>'',
+	'DB_PORT'=>'3306',
+	'DB_CHARSET' => 'utf8',
+	'DB_PREFIX'=>'tc_',//设置表前缀
+
+);
+?>
---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,5 @@
+<?php if (!defined('THINK_PATH')) exit();?><html>
+   <body>
+      <h1>hello world</h1>
+   </body>
+</html>
